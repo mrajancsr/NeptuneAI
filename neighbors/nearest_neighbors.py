63fb011842e73102b13bbfc3bf2ad023ae215c9f
Raj Mani <rajmani@rajs-mbp.myfiosgateway.com>
2022-08-21 14:30:07 -0400

    added some stuff on nearest neighbors classification

--- neighbors/nearest_neighbors.py
+++ neighbors/nearest_neighbors.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import heapq
+import sys
 from dataclasses import dataclass
 from typing import List, Tuple
 
@@ -14,9 +15,27 @@ class kNeighborsClassifier:
     n_neighbors: int
     metric: str = "eucledean"
 
+    def _1NN(
+        self, examples: List[NDArray], targets: NDArray, query: NDArray
+    ) -> List[Tuple[NDArray, NDArray]]:
+        if self.n_neighbors != 1:
+            raise ValueError("Number of neighbors requested is not equal to 1")
+        closest_point = None
+        min_dist = sys.maxsize
+        curr_dist = None
+
+        for index, x in enumerate(examples):
+            curr_dist = norm(x - query)
+            if curr_dist < min_dist:
+                closest_point = (x, targets[index])
+                min_dist = curr_dist
+        return closest_point
+
     def nearest_neighbors(
         self, examples: List[NDArray], targets: NDArray, query: NDArray
     ) -> List[Tuple[NDArray, NDArray]]:
+        if self.n_neighbors == 1:
+            return self._1NN(examples, targets, query)
         heap = []
         for xi, yi in zip(examples, targets):
             dist = norm(xi - query)
