8fa218da1953b989fba588332bd70d5eb943ec34
Raj Mani <rajmani@rajs-mbp.myfiosgateway.com>
2024-02-04 20:39:38 -0500

    added __all__ attribute to ignore flake8 errors

--- neptunelearn/linear_models/__init__.py
+++ neptunelearn/linear_models/__init__.py
@@ -5,3 +5,12 @@ from neptunelearn.linear_models.linear_regression import (
 )
 from neptunelearn.linear_models.logistic_regression import LogisticRegression
 from neptunelearn.linear_models.neural_classifier import AdalineGD, Perceptron
+
+__all__ = [
+    "LogisticRegression",
+    "AdalineGD",
+    "Perceptron",
+    "LinearRegression",
+    "LinearRegressionGD",
+    "LinearRegressionMLE",
+]
